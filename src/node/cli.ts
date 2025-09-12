1e3ed63244b7b16a2a04a98528adb3e793ff3779
Chiran Fernando <pcnfernando@gmail.com>
2025-09-12 18:35:57 +0530

    Add idle-timeout to user args

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -94,6 +94,7 @@ export interface UserProvidedArgs extends UserProvidedCodeArgs {
   "welcome-text"?: string
   "abs-proxy-base-path"?: string
   i18n?: string
+  "idle-timeout"?: number
   /* Positional arguments. */
   _?: string[]
 }
@@ -259,6 +260,7 @@ export const options: Options<Required<UserProvidedArgs>> = {
     type: "string",
     description: "GitHub authentication token (can only be passed in via $GITHUB_TOKEN or the config file).",
   },
+  "idle-timeout": { type: "number", description: "Timeout in minutes to wait before shutting down when idle." },
   "proxy-domain": { type: "string[]", description: "Domain used for proxying ports." },
   "skip-auth-preflight": {
     type: "boolean",
